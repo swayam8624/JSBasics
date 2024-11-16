026d6ed2c5bd30bf48a1ae99c96a747098aea023
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-11-16 04:48:12 +0000

    day2_1

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "swayam.singal@yahoo.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "hahha@gmail.com"
+accountPass = "323232"
+accountCity = "Kolkata"
+
+/* 
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId)
+
+console.table([accountEmail , accountPass , accountId , accountCity])
