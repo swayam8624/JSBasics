8a25901f51909725ade9e6d4d725a6354cd2c2b6
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-11-29 14:08:28 +0000

    04_half

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,4 @@
+console.log(2>1 , 2>=1 , 2<=1 , 2==1 , 2!=1 , "2">"1" , "2">1 , "02">1 , "0">1);
+// can be done for different data types but not encouraged , always compare same datatypes
+
+console.log(null>0 , null==0 , null>=0);
