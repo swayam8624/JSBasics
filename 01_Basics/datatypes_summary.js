b2e7fd70d306dc8ce6d46323b62cf4090d15b791
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-11-30 11:34:31 +0000

    05_half

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,23 @@
+//      Primitive 
+/*
+String
+Number - includes both int and float
+Boolean
+null
+undefined
+Symbol - used to make a value "unique"
+BigInt
+*/
+
+id = Symbol("123")
+// JS is a dynamically typed language (unlike TypeScript)
+anid = Symbol("123")
+console.log(anid==id);
+const bigNum = 23435253121123123123n  // for BigInt
+c
+//      Non-Primitive or Reference
+/*
+Arrays 
+Objects
+Functions
+*/
