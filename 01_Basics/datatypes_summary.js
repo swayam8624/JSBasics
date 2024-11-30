7ec27527f3f3fe5e94bfdda882fdb8694d07c2eb
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-11-30 13:32:46 +0000

    summary_full

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -14,10 +14,37 @@ id = Symbol("123")
 anid = Symbol("123")
 console.log(anid==id);
 const bigNum = 23435253121123123123n  // for BigInt
-c
+
+
 //      Non-Primitive or Reference
 /*
 Arrays 
 Objects
 Functions
+*/
+
+const heros = ["shaktiman" , "nagraj" , "doga"] //return object datatype
+let myobj = {       // return object datatype
+    name : "Swayam",
+    age : 22
+}
+// in value , datatype can be anything
+// functions an also be treated like a variable
+const myfunc = function(){ // return function object datatype
+    console.log("Hello World")
+}
+
+console.log(typeof myfunc , typeof myobj)
+
+// Type of Val                                        Result
+
+// undefined                                          undefined
+// Null                                               object
+// boolean                                            boolean
+// number                                             number
+// String                                             string
+/*
+object(native and does not implement[[Call]])         object
+object(native or host and does implement[[Call]])     function object
+object(host and does not implement[[Call]])           Implementation defined except may not be "undefined" , "boolean" , "number" , "string"
 */
