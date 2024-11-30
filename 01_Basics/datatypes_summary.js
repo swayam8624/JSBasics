316f1428bf3d3bd1ba56a5896e754368a4b62007
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-11-30 13:48:04 +0000

    stackheap

--- 01_Basics/datatypes_summary.js
+++ 01_Basics/datatypes_summary.js
@@ -34,6 +34,7 @@ const myfunc = function(){ // return function object datatype
     console.log("Hello World")
 }
 
+// ---------------------------------------------------------------------------------------
 console.log(typeof myfunc , typeof myobj)
 
 // Type of Val                                        Result
@@ -48,3 +49,31 @@ object(native and does not implement[[Call]])         object
 object(native or host and does implement[[Call]])     function object
 object(host and does not implement[[Call]])           Implementation defined except may not be "undefined" , "boolean" , "number" , "string"
 */
+
+
+
+// ---------------------------------------------------------------------------------------
+
+//                              Memory Allocation
+/*
+Stack -> Primitive Datatypes -- We get a copy of it 
+Heap -> Reference Datatypes -- We get a reference of the original value
+*/
+
+let a = "swa"
+let b = a // we get a copy of it b , not reference
+console.log(b)
+b = "wea"
+console.log(a , b) //a does not get changed as b was just a copy and not reference
+
+let user = {
+    email : "user@gmail.com" , 
+    pass : "hahaha"
+}
+let user2 = user 
+console.log(user , user2)
+user2.pass = "byebyebye"
+console.log(user , user2) // both gets updated as non primitive in heap gives a reference of original value rather than a copy
+// now , the concept to understand is that user and user2 are in stack only as variable 
+// but the content inside it being an object resides in heap
+
