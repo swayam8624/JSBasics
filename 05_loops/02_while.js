52c50765549a607196e43f2117c6592d44ba8ecf
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-12-07 16:08:08 +0000

    while

---
+++ 05_loops/02_while.js
@@ -0,0 +1,10 @@
+// while and do-while
+/*
+while (condition) { //entry controlled ----  while condition is true , execute ,  exit once false
+    
+}
+do {
+    
+} while (condition); // exit controlled ------- execute , check condition , if true , executed next iter untill false
+
+*/
