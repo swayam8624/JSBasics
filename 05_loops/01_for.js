07dcc505ff5276423b09643ec0385ce71519d7b1
swayam singal <132289274+swayam8624@users.noreply.github.com>
2024-12-07 16:05:06 +0000

    for

---
+++ 05_loops/01_for.js
@@ -0,0 +1,15 @@
+// also called iterations
+
+for (let index = 0; index < 10; ++index) {
+    console.log(index)
+}
+// init-condition-block exe-update
+
+// break -> breaks out of for
+// continue -> goes to the top for next iteration skipping the current execution
+
+// nested loops also possible -- everything same as other programming langauges
+// same logic like everywhere
+
+
+
